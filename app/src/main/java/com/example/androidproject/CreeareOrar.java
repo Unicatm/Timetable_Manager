ac8cc71516ebbe61b7d0854211cbc5414de1d1b8
Unicatm <alexandra.tertiscu@gmail.com>
2024-10-12 18:39:27 +0300

    layout pagina Creeare Orar

--- app/src/main/java/com/example/androidproject/CreeareOrar.java
+++ app/src/main/java/com/example/androidproject/CreeareOrar.java
@@ -1,9 +1,6 @@
 package com.example.androidproject;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,30 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button btnCreeareOrar;
+public class CreeareOrar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_creeare_orar);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        btnCreeareOrar =findViewById(R.id.btnStart);
-        btnCreeareOrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CreeareOrar.class);
-                startActivity(intent);
-            }
-        });
-
     }
-
 }
