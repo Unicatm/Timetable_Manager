6b777e6e5bf837328f136b3da718a75191dc38dc
Unicatm <alexandra.tertiscu@gmail.com>
2024-11-03 10:37:56 +0200

    Tema 5 Not Done? - deadline < 7zile se schimba clr card

--- app/src/main/java/com/example/androidproject/customAdapters/AdapterTask.java
+++ app/src/main/java/com/example/androidproject/customAdapters/AdapterTask.java
@@ -1,21 +1,31 @@
 package com.example.androidproject.customAdapters;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Chronometer;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
 import com.example.androidproject.R;
 import com.example.androidproject.clase.Materie;
 import com.example.androidproject.clase.Task;
+import com.google.android.material.card.MaterialCardView;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.List;
 
@@ -35,12 +45,15 @@ public class AdapterTask extends ArrayAdapter<Task> {
         this.layoutInflater=layoutInflater;
     }
 
+    @SuppressLint("ResourceAsColor")
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View view = layoutInflater.inflate(layoutId, parent, false);
 
         Task task = tasks.get(position);
+
+        MaterialCardView card = view.findViewById(R.id.cardTask);
 
         TextView tvDenTask = view.findViewById(R.id.tvDenTask);
         TextView tvDescrTask = view.findViewById(R.id.tvDescTask);
@@ -52,6 +65,23 @@ public class AdapterTask extends ArrayAdapter<Task> {
         tvDescrTask.setText(task.getDescriere());
         tvDenMaterie.setText(task.getDenMaterie());
         tvDeadline.setText(new SimpleDateFormat("dd.MM.yyyy").format(task.getDataDeadline()));
+
+        //TIMP RAMAS DDL
+        LocalDate dataCurenta = LocalDate.now();
+
+        LocalDate deadline = task.getDataDeadline().toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
+
+        long zileRamase = ChronoUnit.DAYS.between(dataCurenta, deadline);
+
+        tvTimeLeft.setText(zileRamase + " zile");
+
+        if(zileRamase<7){
+            card.setCardBackgroundColor(ContextCompat.getColor(this.getContext(),R.color.whitePurple));
+            card.setStrokeColor(ContextCompat.getColor(this.getContext(), R.color.darkPurple));
+            tvTimeLeft.setTextColor(ContextCompat.getColor(this.getContext(), R.color.darkPurple));
+        }
 
 
         return view;
